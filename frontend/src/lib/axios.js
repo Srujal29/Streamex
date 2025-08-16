a61231545e72478a6883b1ee37107a34d3802b63
Srujal29 <srujalvispute2006@gmail.com>
2025-08-16 16:54:23 +0530

    Better Commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Read API base URL from Vite env
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
